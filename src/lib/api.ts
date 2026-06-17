be95ccc2125d83d149f85834bae529471307b685
MD Rasel Ahmed <raselahmed73614@gmail.com>
2026-06-17 17:30:07 +0600

    after production

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://hall-mangement-server.vercel.app";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://hall-management-server-three.vercel.app";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
